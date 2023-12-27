e43acc505d032c12681fb32430c9ec2d340e800e
Vladimir-hash <borovikov_98@list.ru>
2023-12-27 18:49:43 +0300

    refactor code

--- src/AdditionalTasks.java
+++ src/AdditionalTasks.java
@@ -3,30 +3,22 @@ import java.util.Scanner;
 public class AdditionalTasks {
     public static void main(String[] args) {
 //  1. Выведите на кран первые 11 членов последовательности Фибоначчи.
-        int[] numbers = new int[11];
-        int a = 0;
-        int b = 1;
-        numbers[0] = a;
-        numbers[1] = b;
-        for (int i = 0; i < numbers.length; ++i) {
-            if (i == 0) {
-                System.out.println(numbers[i]);
-            } else if  ( i == 1) {
-                System.out.println(numbers[i]);
-            } else {
-                numbers[i] = numbers[i-1] + numbers[i-2];
-                System.out.println(numbers[i]);
-            }
+        int firstNumber = 0;
+        int secondNumber = 1;
+        for(int i =0; i < 11; i++) {
+            System.out.println(firstNumber);
+            firstNumber = secondNumber - firstNumber;
+            secondNumber = secondNumber + firstNumber;
         }
 //  2.  За каждый месяц банк начисляет к сумме вклада 7% от суммы.
 //  Напишите программу, в которую пользователь вводит сумму вклада и количество месяцев.
 //  А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
 //  Для вычисления суммы с  учетом процентов используйте цикл for. Сумма вклада тип float.
         Scanner scanner = new Scanner(System.in);
-        float valueOfMoney = scanner.nextFloat();
+        int valueOfMoney = scanner.nextInt();
         int month = scanner.nextInt();
         for (int i = 1; i <= month; ++i) {
-            valueOfMoney += (float) (valueOfMoney * 0.07);
+            valueOfMoney *= 1.07F;
         }
         System.out.println(valueOfMoney);
 //  3. Напишите программу, которая вывыдит на консоль таблицу умножения.
